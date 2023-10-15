4f32134610a2383d85c28529c2916b85a214720f
qndn3tp <qndn3tp@naver.com>
2023-10-16 02:22:18 +0900

    feat: 탭으로 화면 나누기

--- instagram/lib/main.dart
+++ instagram/lib/main.dart
@@ -10,8 +10,15 @@ void main() {
   );
 }
 
-class MyApp extends StatelessWidget {
+class MyApp extends StatefulWidget {
   const MyApp({super.key});
+
+  @override
+  State<MyApp> createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+  var tab = 0;    // 동적 UI생성: 하단바의 홈, 샵인 경우 화면을 다르게 하도록
 
   @override
   Widget build(BuildContext context) {
@@ -27,10 +34,15 @@ class MyApp extends StatelessWidget {
           )
         ],
       ),
-      body: Text("hello"),
+      body: [Text("홈페이지"), Text("샵페이지")][tab],  // 페이지 나누기
       bottomNavigationBar: BottomNavigationBar(
         showSelectedLabels: false,
         showUnselectedLabels: false,
+        onTap: (i){   // 유저의 선택에 따라 state가 변하도록
+          setState(() {
+            tab = i;
+          });
+        },
         items: [
           BottomNavigationBarItem(icon: Icon(Icons.home_outlined), label: "홈"),
           BottomNavigationBarItem(icon: Icon(Icons.shopping_bag_outlined), label: "샵"),
