d11f0306a3f6622d00d06c452e75a56ad6780b6e
qndn3tp <qndn3tp@naver.com>
2023-10-17 21:33:44 +0900

    feat: 스크롤 내릴 때 더보기 기능 추가

--- instagram/lib/main.dart
+++ instagram/lib/main.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import './style.dart' as style;
 import 'package:http/http.dart' as http;
 import 'dart:convert';
+import 'package:flutter/rendering.dart';
 
 void main() {
   runApp(
@@ -21,7 +22,8 @@ class MyApp extends StatefulWidget {
 
 class _MyAppState extends State<MyApp> {
   var tab = 0;            // 동적 UI생성: 하단바의 홈, 샵인 경우 화면을 다르게 함
-  var data = [];
+  var data = [];          // 서버에서 받은 데이터(글)를 저장
+  var isVisible = 0;      // 하단바를 보이게/보이지 않게 하기 위한 상태
 
   getData() async {       // 서버에서 데이터를 받아옴(get)
     var result = await http.get(Uri.parse("https://codingapple1.github.io/app/data.json"));
@@ -56,7 +58,7 @@ class _MyAppState extends State<MyApp> {
           )
         ],
       ),
-      body: [ Home(data: data), Text("샵페이지")][tab],
+      body: [ Home(data: data, isVisible: isVisible), Text("샵페이지")][tab],
       bottomNavigationBar: BottomNavigationBar(
         showSelectedLabels: false,
         showUnselectedLabels: false,
@@ -75,35 +77,54 @@ class _MyAppState extends State<MyApp> {
 }
 
 // 글 위젯 (사진, 좋아요, 글쓴이, 내용)
-class Home extends StatelessWidget {
-  const Home({super.key, this.data});
+class Home extends StatefulWidget {
+  Home({super.key, this.data, this.isVisible});
 
   final data;
+  var isVisible;
+
+  @override
+  State<Home> createState() => _HomeState();
+}
+
+class _HomeState extends State<Home> {
+
+  var scroll = ScrollController();    // 스크롤 상태를 저장
+
+  @override
+  void initState() {                  // Home 위젯이 로드될 때 실행됨
+    super.initState();
+    scroll.addListener(() {           // 스크롤 상태가 변경될 때마다 실행됨
+      if (scroll.position.pixels == scroll.position.maxScrollExtent) {    // 스크롤 위치가 끝일 때
+        getData();                    // 처음 실행될 때 데이터를 받아옴
+      }
+    });
+  }
+
+  getData() async{
+    var result = await http.get(Uri.parse("https://codingapple1.github.io/app/more1.json"));
+    var result2 = json.decode(result.body);
+    setState(() {
+      widget.data.add(result2);
+    });
+  }
 
   @override
   Widget build(BuildContext context) {
-    if (data.isNotEmpty){
-      return ListView.builder(itemCount: data.length, itemBuilder: (c, i){
+    if (widget.data.isNotEmpty){
+      return ListView.builder(itemCount: widget.data.length, controller: scroll, itemBuilder: (c, i){
         return Column(
           children: [
-            Image.network(data[i]["image"]),
+            Image.network(widget.data[i]["image"]),
             Container(
               margin: EdgeInsets.all(20),
               width: double.infinity,
               child: Column(
                 crossAxisAlignment: CrossAxisAlignment.start,
                 children: [
-                  SizedBox(                 // 좋아요
-                    child: Row(
-                      crossAxisAlignment: CrossAxisAlignment.center,
-                      children: [
-                        Text("좋아요", style: TextStyle(fontWeight: FontWeight.bold),),
-                        Text(data[i]["likes"].toString(), style: TextStyle(fontWeight: FontWeight.bold),),
-                      ],
-                    ),
-                  ),
-                  Text(data[i]["user"]),    // 글쓴이
-                  Text(data[i]["content"]), // 글내용
+                  Text("좋아요 ${widget.data[i]["likes"].toString()}", style: TextStyle(fontWeight: FontWeight.bold),),
+                  Text(widget.data[i]["user"]),    // 글쓴이
+                  Text(widget.data[i]["content"]), // 글내용
                 ],
               ),
             ),
