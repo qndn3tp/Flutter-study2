4af7f444ddff5edb8e263261c100e08221fff755
qndn3tp <qndn3tp@naver.com>
2023-10-18 14:43:33 +0900

    feat: 유저의 폰에 저장된 이미지를 가져오는 기능 추가

--- instagram/lib/main.dart
+++ instagram/lib/main.dart
@@ -2,7 +2,8 @@ import 'package:flutter/material.dart';
 import './style.dart' as style;
 import 'package:http/http.dart' as http;
 import 'dart:convert';
-import 'package:flutter/rendering.dart';
+import 'package:image_picker/image_picker.dart';
+import 'dart:io';
 
 void main() {
   runApp(
@@ -24,6 +25,7 @@ class _MyAppState extends State<MyApp> {
   var tab = 0;                // 동적 UI생성: 하단바의 홈, 샵인 경우 화면을 다르게 함
   var data = [];              // 서버에서 받은 데이터(글)를 저장
   var isVisible = true;       // 하단바를 보이게/보이지 않게 하기 위한 상태
+  var userImage;              // 업로드할 사진의 상태
 
   getData() async {           // 서버에서 데이터를 받아옴(get)
     var result = await http.get(Uri.parse("https://codingapple1.github.io/app/data.json"));
@@ -59,11 +61,18 @@ class _MyAppState extends State<MyApp> {
         title: Text("Instagram"),
         centerTitle: false,
         actions: [
-          IconButton(
-            icon: Icon(Icons.add_box_outlined),        // 새 글 작성 버튼
-            onPressed: (){
+          IconButton(                                  // 새 글 작성 버튼
+            icon: Icon(Icons.add_box_outlined),
+            onPressed: () async {
+              var picker = ImagePicker();              // 이미지를 가져옴
+              var image = await picker.pickImage(source: ImageSource.gallery);    // 유저의 갤러리에 접근해 이미지를 가져옴
+              if (image != null) {
+                setState(() {
+                  userImage = File(image.path);        // 유저가 선택한 이미지 경로를 저장
+                });
+              }
               Navigator.push(context,                  // 새 글 작성 페이지로 이동
-              MaterialPageRoute(builder: (c) => Upload()));
+                  MaterialPageRoute(builder: (c) => Upload(userImage: userImage)));
               },
             iconSize: 30,
           )
@@ -90,7 +99,9 @@ class _MyAppState extends State<MyApp> {
 }
 
 
-// 글 위젯 (사진, 좋아요, 글쓴이, 내용)
+///////////////////////////////////////
+//   글 위젯 (사진, 좋아요, 글쓴이, 내용)   //
+///////////////////////////////////////
 class Home extends StatefulWidget {
   Home({super.key, this.data, this.isVisible, this.isVisibleCallback});
 
@@ -162,15 +173,20 @@ class _HomeState extends State<Home> {
   }
 }
 
-// 새 글 작성 위젯
+///////////////////////////////////////
+//            새 글 작성 위젯           //
+///////////////////////////////////////
 class Upload extends StatelessWidget {
-  const Upload({super.key});
+  const Upload({super.key, this.userImage});
+
+  final userImage;
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         title: Text("새 게시물", style: TextStyle(fontSize: 20),),
+        centerTitle: true,
         leading: IconButton(
           onPressed: (){ Navigator.pop(context); },
           icon: Icon(Icons.close),
@@ -179,11 +195,7 @@ class Upload extends StatelessWidget {
       ),
       body: Column(
         children: [
-          Text("이미지 업로드 화면"),
-          IconButton(
-              onPressed: (){ Navigator.pop(context); },
-              icon: Icon(Icons.close),
-          )
+          Image.file(userImage),
         ],
       ),
     );
