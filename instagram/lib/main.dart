678e919343874b7f4fdea7162186a27c08a09ccd
qndn3tp <qndn3tp@naver.com>
2023-10-18 14:08:25 +0900

    feat: 새 글 작성 페이지 생성(상세페이지)

--- instagram/lib/main.dart
+++ instagram/lib/main.dart
@@ -21,17 +21,17 @@ class MyApp extends StatefulWidget {
 }
 
 class _MyAppState extends State<MyApp> {
-  var tab = 0;            // 동적 UI생성: 하단바의 홈, 샵인 경우 화면을 다르게 함
-  var data = [];          // 서버에서 받은 데이터(글)를 저장
-  var isVisible = true;      // 하단바를 보이게/보이지 않게 하기 위한 상태
+  var tab = 0;                // 동적 UI생성: 하단바의 홈, 샵인 경우 화면을 다르게 함
+  var data = [];              // 서버에서 받은 데이터(글)를 저장
+  var isVisible = true;       // 하단바를 보이게/보이지 않게 하기 위한 상태
 
-  getData() async {       // 서버에서 데이터를 받아옴(get)
+  getData() async {           // 서버에서 데이터를 받아옴(get)
     var result = await http.get(Uri.parse("https://codingapple1.github.io/app/data.json"));
 
     if (result.statusCode == 200) {
       var result2 = json.decode(result.body);
       setState(() {
-        data = result2;     // get한 데이터들을 state에 저장
+        data = result2;       // get한 데이터들을 state에 저장
       });
     } else {
       print("잘못된 페이지입니다");
@@ -42,7 +42,7 @@ class _MyAppState extends State<MyApp> {
   // MyApp 위젯이 로드될 때 실행됨
   void initState() {
     super.initState();
-    getData();            // 처음 실행될 때 데이터를 get 요청
+    getData();                // 처음 실행될 때 데이터를 get 요청
   }
 
   // 스크롤 방향 상태를 전달받는 콜백함수 (하단바를 숨기기위함)
@@ -60,31 +60,35 @@ class _MyAppState extends State<MyApp> {
         centerTitle: false,
         actions: [
           IconButton(
-            icon: Icon(Icons.add_box_outlined),
-            onPressed: (){},
+            icon: Icon(Icons.add_box_outlined),        // 새 글 작성 버튼
+            onPressed: (){
+              Navigator.push(context,                  // 새 글 작성 페이지로 이동
+              MaterialPageRoute(builder: (c) => Upload()));
+              },
             iconSize: 30,
           )
         ],
       ),
       body: [ Home(data: data, isVisibleCallback: updateVisibility), Text("샵페이지")][tab],
-      bottomNavigationBar: isVisible
+      bottomNavigationBar: isVisible    // isVisible의 상태에 따라 하단바를 두거나 숨김
         ? BottomNavigationBar(
         showSelectedLabels: false,
         showUnselectedLabels: false,
-        onTap: (i){       // 유저의 선택에 따라 state가 변하도록
+        onTap: (i){                     // 유저의 선택에 따라 state가 변하도록
           setState(() {
             tab = i;
           });
-        },
+          },
         items: [
           BottomNavigationBarItem(icon: Icon(Icons.home_outlined), label: "홈"),
           BottomNavigationBarItem(icon: Icon(Icons.shopping_bag_outlined), label: "샵"),
         ],
-      )
-          : null
+        )
+          : null                        // 하단바를 숨김
     );
   }
 }
+
 
 // 글 위젯 (사진, 좋아요, 글쓴이, 내용)
 class Home extends StatefulWidget {
@@ -155,5 +159,33 @@ class _HomeState extends State<Home> {
         child: CircularProgressIndicator()
       );
     }
+  }
+}
+
+// 새 글 작성 위젯
+class Upload extends StatelessWidget {
+  const Upload({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("새 게시물", style: TextStyle(fontSize: 20),),
+        leading: IconButton(
+          onPressed: (){ Navigator.pop(context); },
+          icon: Icon(Icons.close),
+          color: Colors.black,
+        ),
+      ),
+      body: Column(
+        children: [
+          Text("이미지 업로드 화면"),
+          IconButton(
+              onPressed: (){ Navigator.pop(context); },
+              icon: Icon(Icons.close),
+          )
+        ],
+      ),
+    );
   }
 }
